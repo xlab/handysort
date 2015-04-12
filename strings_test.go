d1de4833c765442a2d45cb6f9300dbacd34a48d9
Maxim Kouprianov <max@kc.vc>
2015-04-12 21:59:09 +0300

    Swapped case for 00-0

--- strings_test.go
+++ strings_test.go
@@ -29,6 +29,7 @@ func TestStringLess(t *testing.T) {
 		less   bool
 	}{
 		{"0", "00", true},
+		{"00", "0", false},
 		{"aa", "ab", true},
 		{"ab", "abc", true},
 		{"abc", "ad", true},
@@ -43,8 +44,10 @@ func TestStringLess(t *testing.T) {
 		//
 		{"a1b", "a01b", true},
 		{"a01b", "a1b", false},
+
 		{"ab01b", "ab010b", true},
 		{"ab010b", "ab01b", false},
+
 		{"a01b001", "a001b01", true},
 		{"a001b01", "a01b001", false},
 		//
