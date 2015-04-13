fed7569e9401c7a0fff0d9e780e35a562d1cf2a4
Maxim Kouprianov <max@kc.vc>
2015-04-13 17:42:56 +0300

    Added rune lookahead check at the end of digit fields, fixes #3.

--- strings.go
+++ strings.go
@@ -40,14 +40,12 @@ func StringLess(s1, s2 string) (less bool) {
 
 	for i, j := 0, 0; i < len(s1) || j < len(s2); {
 		var r1, r2 rune
-		var w1, w2 int
+		var e1, e2 bool
 		var d1, d2 bool
 
 		// read rune from former string available
-		if i < len(s1) {
-			r1, w1 = utf8.DecodeRuneInString(s1[i:])
-			i += w1
-
+		r1, i, e1 = advanceRune(i, s1)
+		if !e1 {
 			// if digit, accumulate
 			if d1 = ('0' <= r1 && r1 <= '9'); d1 {
 				n1 = append(n1, r1)
@@ -56,10 +54,8 @@ func StringLess(s1, s2 string) (less bool) {
 		}
 
 		// read rune from latter string if available
-		if j < len(s2) {
-			r2, w2 = utf8.DecodeRuneInString(s2[j:])
-			j += w2
-
+		r2, j, e2 = advanceRune(j, s2)
+		if !e2 {
 			// if digit, accumulate
 			if d2 = ('0' <= r2 && r2 <= '9'); d2 {
 				n2 = append(n2, r2)
@@ -71,10 +67,30 @@ func StringLess(s1, s2 string) (less bool) {
 		if (!d1 || !d2) && r1 > 0 && r2 > 0 {
 			if n1NonEmpty && n2NonEmpty {
 				// compare digits in accumulators
-				return compareByDigits(n1, n2)
+				less, greater, equal := compareByDigits(n1, n2)
+				if less {
+					return true
+				} else if greater {
+					return false
+				} else if !equal {
+					return less
+				}
+				// fetch next rune in strings that lack a digit rune
+				if d1 {
+					r1, i, e1 = advanceRune(i, s1)
+				}
+				if d2 {
+					r2, j, e2 = advanceRune(j, s2)
+				}
+				if r1 != r2 {
+					return r1 < r2
+				}
+				// equal runes after digit areas -> continue
+				n1, n2 = n1[0:0], n2[0:0]
+				n1NonEmpty, n2NonEmpty = false, false
 			}
 
-			// detect if non-digit rune from former or latter
+			// just compare non-digit runes
 			if r1 != r2 {
 				return r1 < r2
 			}
@@ -83,18 +99,33 @@ func StringLess(s1, s2 string) (less bool) {
 
 	if n1NonEmpty || n2NonEmpty {
 		// reached both strings ends, compare numeric accumulators
-		if less = compareByDigits(n1, n2); less {
+		less, greater, equal := compareByDigits(n1, n2)
+		if less {
+			return true
+		} else if greater {
+			return false
+		} else if !equal {
 			return less
 		}
-		return len(s1) < len(s2)
 	}
 
 	// last hope
 	return len(s1) < len(s2)
 }
 
-// Compare two numeric fields by their digits
-func compareByDigits(n1, n2 []rune) (less bool) {
+func advanceRune(ptr int, str string) (r rune, i int, end bool) {
+	if ptr < len(str) {
+		var w int
+		r, w = utf8.DecodeRuneInString(str[ptr:])
+		i = ptr + w
+		return
+	}
+	return 0, ptr, true
+}
+
+// Compares two numeric fields by their digits, if equal then
+// compares initial lengths of the numeric fields provided.
+func compareByDigits(n1, n2 []rune) (less, greater, equal bool) {
 	offset := len(n2) - len(n1)
 	n1n2 := offset < 0 // len(n1) > len(n2)
 	if n1n2 {
@@ -120,17 +151,17 @@ func compareByDigits(n1, n2 []rune) (less bool) {
 		r2 := n2[i]
 		if r1 != r2 {
 			if n1n2 {
-				return r2 < r1 // actually r1 < r2
+				return r2 < r1, r1 > r2, false // actually r1 < r2
 			}
-			return r1 < r2
+			return r1 < r2, r2 > r1, false
 		}
 	}
 
 	// numeric value equals, compare by length
 	if n1n2 {
 		// n1 was > n2
-		return false
+		return false, true, true
 	}
 	// eval a comparison only if n1 known to be <= n2
-	return len(n1) < len(n2)
+	return len(n1) < len(n2), len(n2) > len(n1), true
 }
--- strings_test.go
+++ strings_test.go
@@ -6,6 +6,8 @@ import (
 	"sort"
 	"strconv"
 	"testing"
+
+	"github.com/skarademir/naturalsort"
 )
 
 func TestStringSort(t *testing.T) {
@@ -44,15 +46,13 @@ func TestStringLess(t *testing.T) {
 		//
 		{"a1b", "a01b", true},
 		{"a01b", "a1b", false},
-
 		{"ab01b", "ab010b", true},
 		{"ab010b", "ab01b", false},
-
 		{"a01b001", "a001b01", true},
 		{"a001b01", "a01b001", false},
-		//
 		{"a1", "a1x", true},
-		{"a1x", "a1", false},
+		{"1ax", "1b", true},
+		{"1b", "1ax", false},
 		//
 		{"082", "83", true},
 	}
@@ -81,14 +81,22 @@ func BenchmarkHandyStringSort(b *testing.B) {
 	}
 }
 
-func BenchmarkStringLess(b *testing.B) {
+func BenchmarkNaturalStringSort(b *testing.B) {
 	set := testSet(300)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		n := b.N % 999
-		_ = StringLess(set[n][0], set[n+1][1])
+		sort.Sort(naturalsort.NaturalSort(set[b.N%1000]))
 	}
 }
+
+// func BenchmarkStringLess(b *testing.B) {
+// 	set := testSet(300)
+// 	b.ResetTimer()
+// 	for i := 0; i < b.N; i++ {
+// 		n := b.N % 999
+// 		_ = StringLess(set[n][0], set[n+1][1])
+// 	}
+// }
 
 // Get 1000 arrays of 10000-string-arrays.
 func testSet(seed int) [][]string {
